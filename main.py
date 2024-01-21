eeca7278941aa696ed1c1b0f38b51f1ff451c27d
nishant <9211nishant@gmail.com>
2024-01-22 03:16:35 +0530

    correcting score update logic

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ while game_on:
     # collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
-        score.score += 1
         score.update_score()
 
 
--- scoreboard.py
+++ scoreboard.py
@@ -8,9 +8,10 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.color('white')
         self.goto(0, 300)
-        self.update_score()
+        self.write(f"Score: {self.score}", align='center', font=("Verdana", 12, "normal"))
     def update_score(self):
         self.clear()
+        self.score += 1
         self.write(f"Score: {self.score}", align='center', font=("Verdana", 12, "normal"))
     def game_over(self):
         self.goto(0, 0)
